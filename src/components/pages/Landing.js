613cbc9ac3ee220978f792073358a0ab81b0485b
Austin Smith <austinsmith7531@gmail.com>
2019-07-06 20:30:15 -0400

    added the awesome button and a few pictures

--- src/components/pages/Landing.js
+++ src/components/pages/Landing.js
@@ -15,6 +15,8 @@ import {
   Sidebar,
   Visibility
 } from "semantic-ui-react";
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 import { BrowserRouter as Switch, Link } from "react-router-dom";
 
 // Heads up!
@@ -58,10 +60,10 @@ const HomepageHeading = ({ mobile }) => (
     />
     <Switch>
       <Link to="/login">
-        <Button primary size="large">
+        <AwesomeButton primary>
           Get Started
           <Icon name="right arrow" />
-        </Button>
+        </AwesomeButton>
       </Link>
     </Switch>
   </Container>
@@ -116,17 +118,18 @@ class DesktopContainer extends Component {
                 <Menu.Item as="a">About Us</Menu.Item>
                 <Menu.Item as="a" />
                 <Menu.Item position="right">
-                  <Button as="a" inverted={!fixed}>
+                  <AwesomeButton as="a" inverted={!fixed}>
                     Log in
-                  </Button>
-                  <Button
+                  </AwesomeButton>
+                  <AwesomeButton
+                    type="secondary"
                     as="a"
                     inverted={!fixed}
                     primary={fixed}
                     style={{ marginLeft: "0.5em" }}
                   >
                     Sign Up
-                  </Button>
+                  </AwesomeButton>
                 </Menu.Item>
               </Container>
             </Menu>
@@ -192,12 +195,16 @@ class MobileContainer extends Component {
                   <Icon name="sidebar" />
                 </Menu.Item>
                 <Menu.Item position="right">
-                  <Button as="a" inverted>
+                  <AwesomeButton as="a" inverted>
                     Log in
-                  </Button>
-                  <Button as="a" inverted style={{ marginLeft: "0.5em" }}>
+                  </AwesomeButton>
+                  <AwesomeButton
+                    as="a"
+                    inverted
+                    style={{ marginLeft: "0.5em" }}
+                  >
                     Sign Up
-                  </Button>
+                  </AwesomeButton>
                 </Menu.Item>
               </Menu>
             </Container>
@@ -259,12 +266,12 @@ const HomepageLayout = () => (
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign="center">
-            <Button
+            <AwesomeButton
               size="huge"
               href="https://thebestschools.org/careers/best-jobs-for-next-decade/"
             >
               Check Out The Top 25 Careers of 2019
-            </Button>
+            </AwesomeButton>
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -274,38 +281,26 @@ const HomepageLayout = () => (
         <Grid.Row textAlign="center">
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
             <Header as="h3" style={{ fontSize: "2em" }}>
-              "What a Company"
+              Find Yourself Survey
             </Header>
             <p style={{ fontSize: "1.33em" }}>
-              That is what they all say about us
+              Instead of going away to college and putting yourself into debt
+              without being certain about where you want to start, take our
+              personality and career interests survey. It just might point you
+              in a new direction you had not considered!
             </p>
+            <AwesomeButton as="a" type="secondary">
+              Start Survey
+            </AwesomeButton>
           </Grid.Column>
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              <Image avatar src="/images/avatar/large/nan.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
+            <Image src="https://images.unsplash.com/photo-1455734729978-db1ae4f687fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
     <Segment style={{ padding: "8em 0em" }} vertical>
       <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Find Yourself Survey
-        </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of going away to college and putting yourself into debt
-          without being certain about where you want to start, take our
-          personality and career interests survey. It just might point you in a
-          new direction you had not considered!
-        </p>
-        <Button as="a" size="large">
-          Read More
-        </Button>
         <Divider
           as="h4"
           className="header"
@@ -361,14 +356,50 @@ const HomepageLayout = () => (
                 <List.Item as="a">Favorite X-Men</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={7}>
+            <Grid.Column width={4}>
               <Header as="h4" inverted>
-                Footer Header
+                Social Media
               </Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
-              </p>
+              <Container>
+                <AwesomeButton href="https://www.facebook.com" type="facebook">
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="xMidYMid meet"
+                    space="preserve"
+                    viewBox="0 0 50 50"
+                    width="26"
+                    height="24"
+                  >
+                    <path
+                      fill="#FFF"
+                      d="M31.6,44.5V25.7h8l1.3-8h-9.4v-4c0-2.7,1.3-4,4-4h4V1.5c-1.3,0-4.3,0-6.7,0c-6.7,0-9.4,4-9.4,10.7v5.4h-8v8h8v18.8L31.6,44.5L31.6,44.5z"
+                    />
+                  </svg>
+                  Share
+                </AwesomeButton>
+
+                <AwesomeButton
+                  href="https://www.instagram.com"
+                  type="instagram"
+                >
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    preserveAspectRatio="xMidYMid meet"
+                    space="preserve"
+                    viewBox="0 0 50 50"
+                    width="26"
+                    height="24"
+                  >
+                    <path
+                      fill="#FFF"
+                      d="M25,13.3c-5.8,0-10.6,4.7-10.6,10.6S19.1,34.6,25,34.6S35.6,29.8,35.6,24S30.8,13.3,25,13.3z M25,30.7c-3.7,0-6.8-3.1-6.8-6.8s3.1-6.8,6.8-6.8s6.8,3.1,6.8,6.8S28.7,30.7,25,30.7z M36.1,10.6c1.3,0,2.4,1.1,2.4,2.4s-1.1,2.4-2.4,2.4c-1.3,0-2.4-1.1-2.4-2.4S34.7,10.6,36.1,10.6z M42.3,6.8c-2.2-2.2-5.2-3.4-8.7-3.4H16.4c-7.2,0-12,4.8-12,12v17c0,3.6,1.2,6.7,3.5,8.9c2.2,2.2,5.2,3.2,8.6,3.2h17c3.6,0,6.6-1.2,8.7-3.2c2.2-2.2,3.4-5.2,3.4-8.8V15.4C45.6,11.9,44.5,8.9,42.3,6.8z M42,32.6c0,2.6-0.9,4.7-2.4,6.1s-3.6,2.2-6.1,2.2h-17c-2.5,0-4.6-0.7-6.1-2.2s-2.2-3.6-2.2-6.2v-17c0-2.5,0.7-4.6,2.2-6.1C11.8,8,14,7.2,16.5,7.2h17.1c2.5,0,4.6,0.7,6.1,2.2c1.4,1.5,2.2,3.6,2.2,6v17.2H42z"
+                    />
+                  </svg>
+                  Follow Us!
+                </AwesomeButton>
+              </Container>
             </Grid.Column>
           </Grid.Row>
         </Grid>
