54ce2d3807ac91e1eeca566115af589a1b2e8152
Austin Smith <austinsmith7531@gmail.com>
2019-07-13 13:17:21 -0400

    changed the file tree to use a new boilerplate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
-import React from "react";
-import Landing from "./components/pages/Landing";
-import "./App.css";
-import "semantic-ui-css/semantic.min.css";
-import Login from "./components/pages/Login";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react'
+import Home from './screens/Home'
 
-function App() {
-  return (
-    <Router>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/login" component={Login} />
-    </Router>
-  );
-}
+const App = () => (
+  <Home />
+)
 
-export default App;
+export default App
