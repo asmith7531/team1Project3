a4d1e202f7be7a66f5f3faa4e9c23961175906d4
unknown <austinsmith7531@gmail.com>
2019-07-16 05:08:57 -0400

    added some components, TO DO: fix the carousel

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
-// ./src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import './styles.scss'
 import App from './App';
 
-<<<<<<< HEAD
-ReactDOM.render(<AppContainer>
-    <App/>
-  </AppContainer>, document.getElementById('root'));
-
-// Hot Module Replacement API
-if (module.hot) {
-  module.hot.accept();
-=======
 ReactDOM.render(
     <AppContainer>
         <App />
@@ -24,5 +13,4 @@ ReactDOM.render(
 // Hot Module Replacement API
 if (module.hot) {
     module.hot.accept();
->>>>>>> origin
 }
