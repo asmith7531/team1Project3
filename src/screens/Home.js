c0f78cac751054439a16dc80264be99c92a63623
Austin Smith <austinsmith7531@gmail.com>
2019-08-01 17:03:28 -0400

    added the button to the survey, background on homepage

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,24 +4,27 @@ import Carousel from "../components/Carousel";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
 import M from "materialize-css";
-
+import Background from "../../dist/susan-yin-2JIvboGLeho-unsplash.jpg";
 export default class Home extends Component {
   componentDidMount() {
-    var elem = document.querySelector(".carousel");
-    var instance = M.Carousel.init(elem, { duration: 100 });
+    M.AutoInit();
   }
 
   render() {
     return (
       <div>
         <NavBar />
-        <div className="s12 m10 l8">
-          <Carousel />
-        </div>
-        <div className="container">
-          <Card className="left" />
-          <Card className="left" />
-          <Card className="left" />
+        <div className="parallax-container">
+          <br />
+          <br />
+          <br />
+          <div className="s12 m10 l8">
+            <Carousel />
+          </div>
+          <div className="container" />
+          <div className="parallax">
+            <img src={Background} />
+          </div>
         </div>
         <Footer />
       </div>
