b578494397eadc25f0dde9f1220ab6bd38da224b
davidmbedford <david.m.bedford1@gmail.com>
2019-08-14 14:52:59 -0400

    pics now embedded - just need to be formatted

--- src/components/Carousel/Carousel.js
+++ src/components/Carousel/Carousel.js
@@ -3,49 +3,59 @@ import panelone from "../../../dist/geometricPanel.jpg";
 import paneltwo from "../../../dist/graduationPanel.jpg"
 import panelthree from "../../../dist/workplacePanel.jpg";
 import panelfour from "../../../dist/homeofficePanel.jpg";
+import M from "materialize-css";
 
 
 export default class Carousel extends Component {
+  
+  componentDidMount() {
+    M.AutoInit();
+  }
+  
   render() {
     return (
       <>
-        <div className="section row center">
-
-          <div className="slider">
-            <ul className="slides">
-              <li>
-                <img src={panelone}/> 
-                <div className="caption center-align">
-                  <h3>Welcome to ZigZag</h3>
-                  <h5 className="light grey-text text-lighten-3">We're here to help guide you towards a productive and fulfilling future.</h5>
-                </div>
-              </li>
-              <li>
-                <img src={paneltwo}/> 
-                <div className="caption left-align">
-                  <h3>Try our Career Survey</h3>
-                  <h5 className="light grey-text text-lighten-3">Many of us don't know what we want out of our career. But maybe our survey can help you lead you in teh right direction. </h5>
-                </div>
-              </li>
-              <li>
-                <img src={panelthree}/> 
-                <div className="caption right-align">
-                  <h3>Find the perfect School or Job for you</h3>
-                  <h5 className="light grey-text text-lighten-3">Do you already have a path in mind? Our site pulls from a variety of databases to help you take the best next steps</h5>
-                </div>
-              </li>
-              <li>
-                <img src={panelfour}/> 
-                <div className="caption center-align">
-                  <h3>Make a profile!!</h3>
-                  <h5 className="light grey-text text-lighten-3">Taking a new route in life can be disorienting. Make a profile on ZigZag to store all of the most important information you find during your journey.</h5>
-                </div>
-              </li>
-            </ul>
+      <div className="section row center">
+        <div className="carousel carousel-slider">
+          <div className="carousel-fixed-item center col s12 m10 l8" />
+          <div className="carousel-item amber white-text" href="#one!">
+            <h1>Welcome to ZigZag</h1>
+            <h4 className="white-text">
+              We're here to help guide you towards a productive and fulfilling
+              future.
+            </h4>
+            <img src={panelone}/> 
           </div>
-
+          <div className="carousel-item indigo white-text" href="#two!">
+            <h1>Try our Career Survey</h1>
+            <h4 className="white-text">
+              Many of us don't know what wer're doing AND THATS OK. But maybe
+              our survey can help you start your journey...
+            </h4>
+            <img src={paneltwo}/>
+          </div>
+          <div className="carousel-item amber darken-4 white-text" href="#three!">
+            <h1>Find the perfect school or job for you</h1>
+            <h4 className="white-text">
+              Already have a grasp on what you want to do? Our site pulls from
+              a variety of databases to help you find the next steps that are
+              best for you
+            </h4>
+            <img src={panelthree}/>
+          </div>
+          <div className="carousel-item blue white-text" href="#four!">
+            <h1>Make a profile!</h1>
+            <h4 className="white-text">
+              Taking a new route in life can be disorienting. Make a profile
+              on ZigZag to store all of the most important information you
+              find during your journey.
+            </h4>
+            <img src={panelfour}/>
+          </div>
         </div>
-      </>
+      </div>
+    </>
     );
   }
 }
+
